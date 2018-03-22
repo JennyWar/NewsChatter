a3aff82579e800cc499c475b13b894e84db11eb9
Jennifer Guerra <jenniferguerra@MacBook-Pro-2.local>
2018-03-21 18:42:32 -0700

    started restructuring the files

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require('express');
+const router = express.Router();
+// Require all models
+var db = require("../models");
+
+const controller = require('../controllers/index')
+
+// A GET route for scraping the echojs website
+router.get("/scrape", controller.scrape);
+
+// Route for getting all Articles from the db
+router.get("/articles", controller.getArticles);
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", controller.getArticle);
+
+// Route for saving/updating an Article's associated Note
+router.post("/articles/:id", controller.updateArticle);
+
+module.exports = router;
